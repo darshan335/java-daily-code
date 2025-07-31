f684a4d55f41ff00cc34bb73659df14a10c74ad1
Darshan kg <darshan4106@gmail.com>
2025-07-31 12:29:49 +0530

    added updated musicplayer

--- src/Day7/musicplayer.java
+++ src/Day7/musicplayer.java
@@ -4,17 +4,35 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class musicplayer {
     public static void main(String[] args) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
-        String filepath = "C:\\Projects\\java-daily-code\\java-daily-code\\src\\Day7\\Fontana - Ryan Stasik, Kanika Moore.wav";
-        File file = new File(filepath);
-        try (AudioInputStream audiostream = AudioSystem.getAudioInputStream(file)) {
 
+        File file = new File("C:\\Projects\\java-daily-code\\java-daily-code\\src\\Day7\\Fontana - Ryan Stasik, Kanika Moore.wav");
+        try (Scanner scanner = new Scanner(System.in)) {
+            AudioInputStream audiostream = AudioSystem.getAudioInputStream(file);
             Clip clip = AudioSystem.getClip();
             clip.open(audiostream);
-            clip.start();
 
+String response="";
+while(!response.equals("Q")){
+    System.out.println("P=Play");
+    System.out.println("S=Stop");
+    System.out.println("R=Reset");
+    System.out.println("Q=Quit");
+    System.out.print("Enter your choice : ");
+    response=scanner.next().toUpperCase();
+    switch(response){
+        case "Q" ->clip.close();
+        case "S" ->clip.stop();
+        case "R" ->clip.setMicrosecondPosition(0);
+        case "P" ->clip.start();
+        default -> System.out.println("Wrong choice");
+    }
+
+
+}
 
             System.out.println("Loading file...");
         } catch (FileNotFoundException e) {
@@ -30,6 +48,7 @@ public class musicplayer {
         }
         finally {
             System.out.println("Done");
+
         }
     }
 }
